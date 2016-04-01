d01ba5b49f460e8cf6726ea89b7db320ebd6774d
Tatsuman <nakanotatuma@outlook.jp>
2016-04-01 03:14:04 -0700

    Played with Django
    Django is super cool. You can make cms super easily.

---
+++ Coding/Python/Mar31/try_django/env1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nakanotatuma/Desktop/Playground/Coding/Python/Mar31/try_django/env1/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
